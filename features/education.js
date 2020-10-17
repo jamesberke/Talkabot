d73bca3840ab14abe80bc2bcf7976fdbab34afc9
James Berke <james.berke@gmail.com>
2020-10-17 13:57:31 -0700

    import botkit conversations

--- features/education.js
+++ features/education.js
@@ -1,9 +1,20 @@
+const { Botkit, BotkitConversation } = require('botkit');
 const rawData = require('./src/rawData')
+
 
 module.exports = function (controller) {
 
-  controller.on('education', async (bot, message) => {
-    await bot.reply(message, "hi")
-  })
+  // const education = rawData.education.listOfInsitutions;
 
+  // controller.hears('education', 'message,direct_message', async (bot, message) => {
+  //   const initialReply = `I most recently attended ${education[0].institutionName}`;
+  //   await bot.reply(message, initialReply);
+  // });
+
+  // controller.on('message,direct_message', async (bot, message) => {
+  //   await bot.reply(message, `Echo: ${message.text}`);
+  // });
+
+  const MY_DIALOG_ID = 'educationConvo';
+  let edConvo = new BotkitConversation(MY_DIALOG_ID, controller);
 }
--- features/src/rawData.js
+++ features/src/rawData.js
@@ -1,41 +1,53 @@
 module.exports = {
   jobHistory: {
     listOfCompanies: [{
-      companyName: '',
-      jobTitle: '',
-      jobDescription: '',
-      timeLine: ''
+      companyName: 'Storyworld International',
+      jobTitle: 'Software Engineer',
+      jobDescription: 'Built an interactive dashboard and implemented a fully responsive, device-agnostic front end web page using AngularJS, JavaScript, CSS flexbox, and media queries',
+      startDate: '2020-05-25',
+      endDate: '2020-09-15'
     }]
   },
   education: {
     listOfInsitutions: [
       {
-        institutionName: '',
+        institutionName: 'App Academy',
         description: '',
-        timeLine: ''
+        startDate: '2020-05-25',
+        endDate: '2020-09-15'
       },
       {
-        institutionName: '',
+        institutionName: 'City College of San Francisco',
         description: '',
-        timeLine: ''
+        startDate: '2020-05-25',
+        endDate: '2020-09-15'
       }
     ]
-  },    
+  },
   techStack: {
     listOfLanguages: [
       {
-        name: '',
-        yearsCoding: ''
+        name: 'JavaScript',
+        yearsCoding: '2'
       },
       {
-        name: '',
-        yearsCoding: ''
+        name: 'Python',
+        yearsCoding: '3'
+      },
+      {
+        name: 'Ruby',
+        yearsCoding: '2'
       },
     ]
   },
   contactInformation: {
-    email: '',
-    phone: '',
-    socialMediaURL: ''
+    firstName: 'Henry',
+    lastName: 'Ryu',
+    email: 'hkryucr@gmail.com',
+    phone: '510-679-9817',
+    socialMediaURL: '',
+    portfolioURL: 'hkryucr.github.io',
+    linkedInURL: 'linkedin.com/in/henryryume',
+    openToJob: true
   }
 }
