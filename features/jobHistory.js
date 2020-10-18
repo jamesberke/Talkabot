064d7dbc5a922add0e6fd0a03596dbdfcb9e5dd7
unknown <dannythanhphan@gmail.com>
2020-10-18 15:08:13 -0700

    Add replies

--- features/jobHistory.js
+++ features/jobHistory.js
@@ -2,9 +2,11 @@ const { BotkitConversation } = require("botkit");
 const rawData = require("./src/rawData")
 
 module.exports = function (controller) {
+
+  const { listOfCompanies } = rawData.jobHistory;
+  let jobNames = [...listOfCompanies.map(company => company.companyName)]
   
   controller.hears(['#JobHistory', async(message) => message.text.toLowerCase().includes("job")], ['message','direct_message'], async function(bot, message) {
-    const { listOfCompanies } = rawData.jobHistory;
 
     if (listOfCompanies.length === 1) {
       
@@ -48,7 +50,6 @@ module.exports = function (controller) {
         })
       }, 5000)
     } else {
-      let jobNames = [...listOfCompanies.map(company => company.companyName)]
 
       await bot.reply(message, { type: 'typing' });
 
@@ -64,30 +65,47 @@ module.exports = function (controller) {
           })
         })
       }, 1000);
-      
-      controller.hears(jobNames, ['message','direct_message'], async function(bot, message) {
-        let mainCompany = listOfCompanies.filter(company => company.companyName === message.text);
-        await bot.reply(message, { type: 'typing' });
-
-        setTimeout(async() => {
-          await bot.changeContext(message.reference);
-          await bot.reply(message, {
-            text: `<div>I worked at ${mainCompany[0].companyName} as a 
-              ${mainCompany[0].jobTitle} from ${mainCompany[0].startDate.slice(5,7)}/${mainCompany[0].startDate.slice(0,4)}
-               to ${mainCompany[0].endDate.slice(5, 7)}/${mainCompany[0].endDate.slice(0,4)}.</div>`
-          });
-          await bot.reply(message, { type: 'typing' });
-        }, 1000);
-    
-        setTimeout(async() => {
-          await bot.changeContext(message.reference);
-          await bot.reply(message, {
-            text: `<div>I ${mainCompany[0].jobDescription}.</div>`
-          });
-        }, 3000);
-      }); 
-      
     }
+
+    controller.hears(jobNames, ['message','direct_message'], async function(bot, message) {
+      let mainCompany = listOfCompanies.filter(company => company.companyName === message.text);
+      await bot.reply(message, { type: 'typing' });
+
+      setTimeout(async() => {
+        await bot.changeContext(message.reference);
+        await bot.reply(message, {
+          text: `<div>I worked at ${mainCompany[0].companyName} as a 
+            ${mainCompany[0].jobTitle} from ${mainCompany[0].startDate.slice(5,7)}/${mainCompany[0].startDate.slice(0,4)}
+              to ${mainCompany[0].endDate.slice(5, 7)}/${mainCompany[0].endDate.slice(0,4)}.</div>`
+        });
+        await bot.reply(message, { type: 'typing' });
+      }, 1000);
+  
+      setTimeout(async() => {
+        await bot.changeContext(message.reference);
+        await bot.reply(message, {
+          text: `<div>I ${mainCompany[0].jobDescription}.</div>`
+        });
+        await bot.reply(message, { type: 'typing' });
+      }, 3000);
+
+      setTimeout(async() => {
+        await bot.changeContext(message.reference);
+        await bot.reply(message, { 
+          text: `Is there anything else you'd like to learn about me? You can choose between the buttons below or type <strong>#Contact, #Experience, or #TechStack</strong>`,
+          quick_replies: [
+              {
+                  title: 'Other Jobs',
+                  payload: 'other jobs',
+              },
+              {
+                  title: "Let's move on",
+                  payload: "Let's move on"
+              }
+          ]
+        })
+      }, 5000)
+    }); 
   });
 
 }
