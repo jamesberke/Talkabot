1a2830c1070da0786f2c635478a1bf87396535b5
James Berke <james.berke@gmail.com>
2020-10-18 14:54:11 -0700

    switched dialog pieces to first person

--- features/introduction.js
+++ features/introduction.js
@@ -3,7 +3,7 @@ const rawData = require('./src/rawData')
 module.exports = function(controller) {
     controller.on('welcome_back', async(bot, message) => {
         const { firstName, openToJob } = rawData.contactInformation
-        const jobOpportunity = openToJob ? `<strong>${firstName}</strong> is currently <strong>open</strong> to a new opportunity` : `${firstName} is not curently looking for an opportunity`
+        const jobOpportunity = openToJob ? `I am currently <strong>open</strong> for a new opportunity` : `I am not curently looking for a new opportunity`
         await bot.reply(message, `Hi there, my name is <strong>${firstName}</strong>`)
         await bot.reply(message, `${jobOpportunity}`)
         await bot.reply(message, { 
