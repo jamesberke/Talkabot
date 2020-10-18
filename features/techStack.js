de53e9f7f5724c0463117957b3b376d1c98ecff3
James Berke <james.berke@gmail.com>
2020-10-18 13:45:11 -0700

    set timeouts for techStach dialog branch

--- features/techStack.js
+++ features/techStack.js
@@ -11,6 +11,9 @@ const listTechStackObjs = [...listOfLanguages.map(el => ({
   payload: el.name
 }))]
 
+
+
+
 module.exports = function (controller) {
   // // use a function to match a condition in the message
   // controller.hears(async (message) => message.text && message.text.toLowerCase() === 'foo', ['message'], async (bot, message) => {
@@ -24,27 +27,66 @@ module.exports = function (controller) {
 
   // match any one of set of mixed patterns like a string, a regular expression
   controller.hears(['#techStack', new RegExp(/^TechStack/)], ['message', 'direct_message'], async function (bot, message) {
-      await bot.reply(message,{ text: `Here is the list of <Strong>Henry's Tech Stack</Strong>` });
+
+    await bot.reply(message, { type: 'typing' });
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, { text: `Here is the list of <Strong>Henry's Tech Stack</Strong>` });
+      await bot.reply(message, { type: 'typing' });
+    }, 1000);
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
       await bot.reply(message, {
         text: `<div><Strong>Languages</Strong></div>${listOfLanguages.map(lang => `<div>- ${lang.name}: ${lang.yearsCoding} years</div>`).join('')}`
       });
+      await bot.reply(message, { type: 'typing' });
+    }, 3000);
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
       await bot.reply(message, {
         text: `<div><Strong>Technologies</Strong></div>${listOfTechnologies.map(lang => `<div>- ${lang.name}: ${lang.yearsCoding} years</div>`).join('')}`
       });
+      await bot.reply(message, { type: 'typing' });
+    }, 5000);
+      
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
       await bot.reply(message, {
         text: `Would you like to see ${firstName}'s projects related to the specific technology?`,
         quick_replies: listTechStackObjs
       })
+    }, 6000);
+      
+      
   });
 
   controller.hears(listTechStacks, ['message'], async function (bot, message) {
     const selectedProjects = listOfProjects.filter(project => project.technologies.includes(message.text))
-    await bot.reply(message, {
-      text: `Here is the list of the related projects`,
-    })
-    await bot.reply(message, {
-      text: `<div>${selectedProjects.map(project=>`<div>- <a href="${project.url}" target="_blank">${project.name}</a></div>`).join('')}</div>`
-    });
+
+    await bot.reply(message, { type: 'typing' });
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, {
+        text: `Here is the list of the related projects`,
+      })
+      await bot.reply(message, { type: 'typing' });
+    }, 1000);
+    
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, {
+        text: `<div>${selectedProjects.map(project => `<div>- <a href="${project.url}" target="_blank">${project.name}</a></div>`).join('')}</div>`,
+        quick_replies: [{
+          title: "Let's move on",
+          payload: "Let's move on"
+        }]
+      });
+    }, 3000);
+    
 
   });
 
