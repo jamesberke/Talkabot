787af1401874f002f68d3d3c511cf9cbbcbc71ca
James Berke <james.berke@gmail.com>
2020-10-17 19:37:43 -0700

    added app academy tree

--- features/education.js
+++ features/education.js
@@ -7,54 +7,54 @@ module.exports = function (controller) {
   let convo = new BotkitConversation('educationDialogue', controller);
   const education = rawData.education.listOfInsitutions;
 
-  controller.hears(['education', 'school'], 'message,direct_message', async (bot, message) => {
-    // let convo = new BotkitConversation('educationDialogue', controller);
+  controller.hears(['education', 'school'], ['message','direct_message'], async (bot, message) => {
     const initialReply = listEducation(education);
     await bot.reply(message, initialReply);
-
-    await bot.ask('Would you like to learn more about my time at one of these schools?', [
-      {
-        pattern: 'yes',
-        type: 'string',
-        handler: async (response_text, convo, bot, full_message) => {
-          return await convo.gotoThread('yes_taco');
-        }
-      },
-      {
-        pattern: 'no',
-        type: 'string',
-        handler: async (response_text, convo, bot, full_message) => {
-          return await convo.gotoThread('no_taco');
-        }
-      },
-      {
-        default: true,
-        handler: async (response_text, convo, bot, full_message) => {
-          await bot.say('I do not understand your response!');
-          // start over!
-          return await convo.repeat();
-        }
-      }
-    ], { key: 'tacos' });
+    await bot.reply(message, {
+            text: 'Would you like to learn more about one of these schools?',
+            quick_replies:[{title: 'App Academy',
+                            payload: 'App Academy'},
+                          {title: 'City College of San Francisco',
+                            payload: 'City College of San Francisco'}]
+    })
   });
 
-  controller.on('message,direct_message', async (bot, message) => {
-    await bot.reply(message, `Echo: ${message.text}`);
-  });
-
+  controller.hears('App Academy', ['message', 'direct_message'], async (bot, message) => {
+    const appAcademy = education.find(school => school.institutionName === 'App Academy');
+    const aAreply = parseDescription(appAcademy)
+    await bot.reply(message, aAreply);
   
-}
+  });
 
+  const listEducation = function (schools) {
+    const schoolNames = schools.map(school => school.institutionName)
 
+    if (schools.length <= 1) {
+      return `I most recently attended ${schoolNames[0]}`
+    } else {
+      const lastSchool = schoolNames.pop();
+      return `I have attended ${schoolNames.join(" ,")} and ${lastSchool}`
+    }
+  };
 
+  const parseDescription = function (school) {
+    return `${school.description} I attended for ${getLengthAtSchool(school.startDate, school.endDate)} and graduated in ${school.endDate[1]}`
+  };
 
-const listEducation = function (schools) {
-  const schoolNames = schools.map(school => school.institutionName)
+  const getLengthAtSchool = function (startDate, endDate) {
+    const monthsWorking = 12 - (startDate[0] - endDate[0]);
+    let yearsWorking = endDate[1] - startDate[1];
 
-  if (schools.length <= 1) {
-    return `I most recently attended ${schoolNames[0]}`
-  } else {
-    const lastSchool = schoolNames.pop();
-    return `I have attended ${schoolNames.join(" ,")} and ${lastSchool}`
+    if (endDate[0] < startDate[0]) yearsWorking -= 1;
+
+    if (yearsWorking) {
+      return `${yearsWorking} years and ${monthsWorking} months`;
+    } else {
+      return `${monthsWorking} months`;
+    };
   }
+
+
 }
+
+
