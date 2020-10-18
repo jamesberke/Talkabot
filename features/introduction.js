88cf3a7ce2f9bfa6ca0b90c543ffe9523eaf89bf
unknown <dannythanhphan@gmail.com>
2020-10-18 10:45:34 -0700

    Update introduction

--- features/introduction.js
+++ features/introduction.js
@@ -4,10 +4,10 @@ module.exports = function(controller) {
     controller.on('welcome_back', async(bot, message) => {
         const { firstName, openToJob } = rawData.contactInformation
         const jobOpportunity = openToJob ? `<strong>${firstName}</strong> is currently <strong>open</strong> to a new opportunity` : `${firstName} is not curently looking for an opportunity`
-        await bot.reply(message, `Hi there, welcome to <strong>${firstName}</strong>'s ChatBot`)
+        await bot.reply(message, `Hi there, my name is <strong>${firstName}</strong>`)
         await bot.reply(message, `${jobOpportunity}`)
         await bot.reply(message, { 
-            text: `Get more information about ${firstName} by typing <strong>#Contact, #JobHistory, #Experience, #TechStack, etc.</strong>`,
+            text: `To learn more about me, click one of the buttons below or type <strong>#Contact, #JobHistory, #Experience, #TechStack, etc.</strong>`,
             quick_replies: [
                 {
                     title: 'Contact',
