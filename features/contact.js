fe3f310d204f909757a101333a7c03cea1c84f23
James Berke <james.berke@gmail.com>
2020-10-18 14:41:04 -0700

    began work on contact dialog

--- features/contact.js
+++ features/contact.js
@@ -1,6 +1,33 @@
 const { BotkitConversation } = require("botkit");
+const rawData = require('./src/rawData')
+const { contactInformation } = rawData.contactInformation;
+
 
 module.exports = function (controller) {
 
+    controller.hears('conact', ['message', 'direct_message'], async (bot, message) => {
+
+        await bot.reply(message, { type: 'typing' });
+
+        setTimeout(async () => {
+            await bot.changeContext(message.reference);
+            await bot.reply(message, {
+                text: 'Would you like to learn more about one of these schools?',
+                quick_replies: [{
+                    title: 'App Academy',
+                    payload: 'App Academy'
+                },
+                {
+                    title: 'City College of San Francisco',
+                    payload: 'City College of San Francisco'
+                },
+                {
+                    title: "Let's move on",
+                    payload: "Let's move on"
+                }]
+            });
+        }, 1000);
+
+    });
     
 };
