12bb19fef686e3201c5888fa7e29dde108988431
James Berke <james.berke@gmail.com>
2020-10-18 19:36:04 -0700

    removed test javascript files

--- features/help.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function (controller) {
-  controller.hears(['#help', new RegExp(/^#help/)], ['message'], async (bot, message) => {
-    await bot.reply(message, {
-      text: `Here are the following commands that you can use. `,
-    })
-  })
-}
